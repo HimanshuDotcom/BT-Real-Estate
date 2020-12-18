4c38fbe10d58e4a51847150a4b2bc5e6f92ca91b
Himanshu <arorahimanshu472@gmail.com>
2020-12-18 22:42:01 +0530

    search form done

--- listings/views.py
+++ listings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Listing
+from .choices import bedroom_choices, price_choices 
 # Create your views here.
 
 def index(request):
@@ -23,4 +24,39 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    #keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords: # to check empty string
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+    
+    #city
+    if 'city'in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact= city) # iexact case insensitive exact in case sensitive
+    
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state: # to check empty string
+            queryset_list = queryset_list.filter(state__iexact=state)
+    
+    #bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms: # to check empty string
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+    
+    #price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price: # to check empty string
+            queryset_list = queryset_list.filter(price__lte=price)
+    context = {
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'listings': queryset_list
+    }
+    return render(request, 'listings/search.html', context)
