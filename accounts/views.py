10132b69c654f86c0ba5d71431bdccf5c9d06e50
Himanshu <arorahimanshu472@gmail.com>
2020-12-21 12:25:14 +0530

    messages app added

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,41 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib import messages
 
 # Create your views here.
 def login(request):
     return render(request,'accounts/login.html')
 
 def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        #check password
+        if password == password2:
+            # check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'username is already taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request,'email has used already')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username = username, password = password, email = email, 
+                    first_name= first_name, last_name = last_name)
+                    # auth.login(request, user)
+                    # return redirect('index')
+                    user.save()
+                    return redirect('login')
+        else:
+            messages.error(request,'password does not match')
+            return redirect('register')
     return render(request, 'accounts/register.html')
 
 def logout(request):
--- btre/static/js/main.js
+++ btre/static/js/main.js
@@ -1,2 +1,7 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
+
+///python manage.py collectstatic to run this
+setTimeout(() => {
+    $('#message').fadeOut('slow')
+}, 2000);
